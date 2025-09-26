8c0565ae221f48ed6e297a23e2d4c0ce7ba86b97
Blueman3612 <nathan.hall87@gmail.com>
2025-09-25 20:52:56 -0500

    Refactor audio management to implement a pool of AudioStreamPlayers for sound effects
    - Replaced single AudioStreamPlayer with a pool to handle multiple simultaneous sound effects.
    - Added functionality to retrieve an available AudioStreamPlayer from the pool, improving sound management.
    - Updated sound effect playback methods to utilize the new player pool, ensuring smoother audio experiences during gameplay.

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -10,15 +10,18 @@ var close_sound = preload("res://assets/sounds/Close.wav")
 # Preloaded music resources
 var background_music = preload("res://assets/music/Split.wav")
 
-# AudioStreamPlayer for SFX and Music
-var sfx_player: AudioStreamPlayer
+# AudioStreamPlayer pool for SFX and Music
+var sfx_player_pool: Array[AudioStreamPlayer] = []
 var music_player: AudioStreamPlayer
+var max_sfx_players = 8  # Maximum number of simultaneous sound effects
 
 func _ready():
-	# Create AudioStreamPlayer for SFX
-	sfx_player = AudioStreamPlayer.new()
-	sfx_player.bus = "SFX"  # Use the SFX audio bus
-	add_child(sfx_player)
+	# Create pool of AudioStreamPlayer instances for SFX
+	for i in range(max_sfx_players):
+		var player = AudioStreamPlayer.new()
+		player.bus = "SFX"  # Use the SFX audio bus
+		add_child(player)
+		sfx_player_pool.append(player)
 	
 	# Create AudioStreamPlayer for Music
 	music_player = AudioStreamPlayer.new()
@@ -31,35 +34,51 @@ func _ready():
 		music_player.play()
 		music_player.finished.connect(_on_music_finished)
 
+# Get an available AudioStreamPlayer from the pool
+func get_available_sfx_player() -> AudioStreamPlayer:
+	# First, try to find a player that's not currently playing
+	for player in sfx_player_pool:
+		if not player.playing:
+			return player
+	
+	# If all players are busy, return the first one (it will be interrupted)
+	# This provides a fallback in case we exceed max_sfx_players simultaneous sounds
+	return sfx_player_pool[0]
+
 # Play tick sound when user inputs/backspaces characters
 func play_tick():
 	if tick_sound:
-		sfx_player.stream = tick_sound
-		sfx_player.play()
+		var player = get_available_sfx_player()
+		player.stream = tick_sound
+		player.play()
 
 # Play correct sound when user submits correct answer
 func play_correct():
 	if correct_sound:
-		sfx_player.stream = correct_sound
-		sfx_player.play()
+		var player = get_available_sfx_player()
+		player.stream = correct_sound
+		player.play()
 
 # Play incorrect sound when user submits incorrect answer
 func play_incorrect():
 	if incorrect_sound:
-		sfx_player.stream = incorrect_sound
-		sfx_player.play()
+		var player = get_available_sfx_player()
+		player.stream = incorrect_sound
+		player.play()
 
 # Play get sound when player earns a star
 func play_get():
 	if get_sound:
-		sfx_player.stream = get_sound
-		sfx_player.play()
+		var player = get_available_sfx_player()
+		player.stream = get_sound
+		player.play()
 
 # Play close sound when player doesn't earn a star
 func play_close():
 	if close_sound:
-		sfx_player.stream = close_sound
-		sfx_player.play()
+		var player = get_available_sfx_player()
+		player.stream = close_sound
+		player.play()
 
 # Callback function to restart music when it finishes (for looping)
 func _on_music_finished():
